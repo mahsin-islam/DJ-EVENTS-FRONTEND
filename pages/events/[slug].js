6ca84c02a7bbf15fdab43dd3736b49a67bf9d869
MD. MAHSIN UL ISLAM <mahsin.islam@gmail.com>
2022-03-18 12:04:26 +0600

    Pages and Routing

---
+++ pages/events/[slug].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function EventPage() {
+  return (
+    <div>
+      <h1>My Event Slug</h1>
+      <p>This is an app to find the latest DJ and other musical events.</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hello!
         </h1>
 
         <p className={styles.description}>
